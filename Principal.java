af28bb5b4fd196bff264ef3cbadc8eccea03768e
OnetOFuX <132602978+OnetOFuX@users.noreply.github.com>
2024-06-10 08:33:08 -0500

    Update Principal.java

--- Principal.java
+++ Principal.java
@@ -2,7 +2,9 @@ public class Principal {
     public static void main(String[] args) {
 
         DAVID david = new DAVID();
+        ronaldo ronaldo = new ronaldo();
 //        menuLoginAndcarrito menu = new menuLoginAndcarrito();
+        ronaldo.iniciar();
         david.Inkafarma();
         david.Crearcuenta();
         david.Login();
